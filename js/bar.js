a1ed85c0bfddc8d6305dcdc0254567824cb938ef
ステラ <live.hylia@outlook.com>
2023-10-11 15:21:45 -0400

    Create bar.js

---
+++ js/bar.js
@@ -0,0 +1,14 @@
+function CreateSnackbar(txt, timeToStart, timeToRemove) {
+        setTimeout(() => {
+        document.getElementById(
+          "snackbar"
+        ).innerHTML = `${txt || '<i class="bi bi-heart-fill" style="color: red; font-size: 20px;"></i> This website is <a href="https://git.hylia.dev/hylia.dev" target="_blank">open source</a>.' }`;
+        // Remove the class hid, and add the class show
+        document.getElementById("snackbar").classList.remove("d-none");
+        document.getElementById("snackbar").classList.add("show");
+
+        setTimeout(function () {
+          document.body.removeChild(document.getElementById("snackbar"));
+        }, timeToRemove || 3000);
+      }, timeToStart || 2000);
+}
